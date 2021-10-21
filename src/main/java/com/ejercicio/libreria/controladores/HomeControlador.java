b0a3fc475431d76007bbeed3b0e6de7a71794d76
EmaMuises <emanuelmuises95@gmail.com>
2021-10-20 23:14:03 -0300

    Iniciacion parte Front-End

--- src/main/java/com/ejercicio/libreria/controladores/HomeControlador.java
+++ src/main/java/com/ejercicio/libreria/controladores/HomeControlador.java
@@ -1,8 +1,17 @@
 package com.ejercicio.libreria.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeControlador {
 
+	@GetMapping("/home")
+	public String home(ModelMap modelo) {
+		return "home";
+	}
+	
 }
